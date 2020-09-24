feb17c11fbbc63e5e3e8a099c622c31b86b23053
Nicholas <nicholasgust@gmail.com>
2020-09-24 13:50:14 -0700

    secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   config.action_mailer.default_url_options = { :host => "fallproplan.com" }
   config.action_mailer.asset_host = 'https://fallproplan.com'
 
